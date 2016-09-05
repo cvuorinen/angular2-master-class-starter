e2a5d4d856931845e8b510e19939d36205f54cea
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-09-05 10:45:53 +0300

    Exercise 1: Using Directives
    https://github.com/thoughtram/angular2-master-class-exercise-descriptions/blob/master/exercises/jump-start-tutorial/exercise-1_using-directives.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
+//import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
+import { ContactsHeaderComponent } from './contacts-header'; // shorthand works because exported in contacts-header/index.ts
 
 @NgModule({
-  declarations: [ContactsAppComponent],
+  declarations: [
+    ContactsAppComponent,
+    ContactsHeaderComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [ContactsAppComponent]
 })
