2a66c1ec2627eb71d0f42055e047c4f747db88f1
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-09-05 14:21:22 +0300

    Exercise 5: Route to your first component
    https://github.com/thoughtram/angular2-master-class-exercise-descriptions/blob/master/exercises/jump-start-tutorial/exercise-5_route-to-contactslistcomponent.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ContactsAppComponent } from './contacts.component';
 //import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
 import { ContactsHeaderComponent } from './contacts-header'; // shorthand works because exported in contacts-header/index.ts
 import { ContactsService } from './contacts.service';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactsAppRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     ContactsAppComponent,
-    ContactsHeaderComponent
+    ContactsHeaderComponent,
+    ContactsListComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+      BrowserModule,
+      RouterModule.forRoot(ContactsAppRoutes)
+  ],
   bootstrap: [ContactsAppComponent],
   providers: [ContactsService]
 })
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact } from './models/contact';
-import { ContactsService } from './contacts.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css']
 })
-export class ContactsAppComponent implements OnInit {
-    public contacts: Contact[];
-
-    constructor(private contactsService: ContactsService) {}
-
-    ngOnInit(): void {
-        this.contacts = this.contactsService.getContacts();
-    }
+export class ContactsAppComponent {
+    constructor() {}
 }
